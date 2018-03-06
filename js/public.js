dd9fc96588dd737a5ab4b4f140456d057403696d
SaSaChu <teresa7397@gmail.com>
2018-03-06 12:52:47 +0800

    桌機over

--- js/public.js
+++ js/public.js
@@ -1,10 +1,13 @@
 /**
- * @author      OA Wu <comdan66@gmail.com>
+ * @author      Zeus Design <service@zeusdesign.com.tw>
  * @copyright   Copyright (c) 2015 - 2018, OAF2E
  * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
  */
  
 $(function () {
+
+  $('.c_btn').click(function() {
+    $('.open_boxs').toggleClass('show');
+  });
 
 });
