10367f4d05bd7672b9e44a4dc4f641a07d5bf1cc
SaSaChu <teresa7397@gmail.com>
2018-03-06 14:39:58 +0800

    Minify js、html, fix gh-pages path bug.

--- js/public.js
+++ js/public.js
@@ -1,13 +1,1 @@
-/**
- * @author      Zeus Design <service@zeusdesign.com.tw>
- * @copyright   Copyright (c) 2015 - 2018, OAF2E
- * @license     http://opensource.org/licenses/MIT  MIT License
- */
- 
-$(function () {
-
-  $('.c_btn').click(function() {
-    $('.open_boxs').toggleClass('show');
-  });
-
-});
+$(function(){$(".c_btn").click(function(){$(".open_boxs").toggleClass("show")})});
